4211cda5ff4553e6a60e3660941ad6f26f839672
Ajay Jaganathan <ajayjeganathan@gmail.com>
2019-09-19 16:21:56 +0530

    add the changing tabs in instance detail page

--- src/components/CardComponent/CardBig.tsx
+++ src/components/CardComponent/CardBig.tsx
@@ -4,14 +4,18 @@ import { TextContent, Text, TextVariants } from '@patternfly/react-core';
 import { CubesIcon } from '@patternfly/react-icons';
 import { Title, EmptyState, EmptyStateVariant, EmptyStateIcon, EmptyStateBody } from '@patternfly/react-core';
 export interface IOwnProps {}
-export interface IStateProps {}
+export interface IStateProps {
+  tabDetails: string;
+}
 export default class CardBig extends React.Component<IOwnProps, IStateProps> {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      tabDetails: ''
+    };
   }
-  handleOnClick = () => {
-    alert('tab');
+  handleOnClick = tabDetail => event => {
+    this.setState({ tabDetails: tabDetail });
   };
   render() {
     return (
@@ -19,35 +23,74 @@ export default class CardBig extends React.Component<IOwnProps, IStateProps> {
         <CardHeader>
           <div className="pf-l-flex pf-m-justify-content-space-evenly pf-m-align-items-center pf-m-align-content-center">
             <TextContent>
-              <Text component={TextVariants.a} href="#" onClick={this.handleOnClick}>
+              <Text component={TextVariants.a} href="#" onClick={this.handleOnClick('Process Diagram')}>
                 Process Diagram
               </Text>
             </TextContent>
             <TextContent>
-              <Text component={TextVariants.a} href="#">
+              <Text component={TextVariants.a} href="#" onClick={this.handleOnClick('Process Log Events')}>
                 Process Log Events
               </Text>
             </TextContent>
             <TextContent>
-              <Text component={TextVariants.a} href="#">
+              <Text component={TextVariants.a} href="#" onClick={this.handleOnClick('Variables')}>
                 Variables
               </Text>
             </TextContent>
           </div>
         </CardHeader>
         <hr className="pf-c-divider"></hr>
-        <CardBody>
-          {' '}
-          <EmptyState variant={EmptyStateVariant.full}>
-            <EmptyStateIcon icon={CubesIcon} />
-            <Title headingLevel="h5" size="lg">
-              Process Diagram To Be Added Dynamically
-            </Title>
-            <EmptyStateBody>
-              The ProcessDiagram, Log Events and Variables would appear here base on the tab clicked.
-            </EmptyStateBody>
-          </EmptyState>
-        </CardBody>
+        {(() => {
+          if (this.state.tabDetails == 'Process Diagram') {
+            return (
+              <CardBody>
+                <EmptyState variant={EmptyStateVariant.full}>
+                  <EmptyStateIcon icon={CubesIcon} />
+                  <Title headingLevel="h5" size="lg">
+                    Process Diagram To Be Added Dynamically
+                  </Title>
+                  <EmptyStateBody>The Process Diagram would appear here based on the tab clicked.</EmptyStateBody>
+                </EmptyState>
+              </CardBody>
+            );
+          } else if (this.state.tabDetails == 'Process Log Events') {
+            return (
+              <CardBody>
+                <EmptyState variant={EmptyStateVariant.full}>
+                  <EmptyStateIcon icon={CubesIcon} />
+                  <Title headingLevel="h5" size="lg">
+                    Process Log Events To Be Added Dynamically
+                  </Title>
+                  <EmptyStateBody>The Process Log Events would appear here based on the tab clicked.</EmptyStateBody>
+                </EmptyState>
+              </CardBody>
+            );
+          } else if (this.state.tabDetails == 'Variables') {
+            return (
+              <CardBody>
+                <EmptyState variant={EmptyStateVariant.full}>
+                  <EmptyStateIcon icon={CubesIcon} />
+                  <Title headingLevel="h5" size="lg">
+                    Variables To Be Added Dynamically
+                  </Title>
+                  <EmptyStateBody>Variables would appear here based on the tab clicked.</EmptyStateBody>
+                </EmptyState>
+              </CardBody>
+            );
+          } else {
+            return (
+              <CardBody>
+                <EmptyState variant={EmptyStateVariant.full}>
+                  <EmptyStateIcon icon={CubesIcon} />
+                  <Title headingLevel="h5" size="lg">
+                    Process Diagram To Be Added Dynamically
+                  </Title>
+                  <EmptyStateBody>The Process Diagram would appear here based on the tab clicked.</EmptyStateBody>
+                </EmptyState>
+              </CardBody>
+            );
+          }
+        })()}
       </Card>
     );
   }
